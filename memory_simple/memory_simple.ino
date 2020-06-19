7e617958fd9e0b44e8600a7b4294728ae28bfc70
Tim Havelka <tim.havelka@gmail.com>
2020-06-19 14:02:34 -0700

    Add new fibonacci program

--- memory_simple/memory_simple.ino
+++ memory_simple/memory_simple.ino
@@ -39,6 +39,7 @@ byte ROM[64] = {
 /* 
  * I/O and math checkout. Reads in a value from input port 0, then repeatedly increments accumulator 
  * and writes to output port 0. 
+ * Requires input and output at port 0
  */
 //byte ROM[64] = {
 //  0xDB, 0x00,       // IN A, 0: Load val from port 0 into accumulator (11011011, 00000000)
@@ -51,6 +52,7 @@ byte ROM[64] = {
  * Memory checkout. Reads a value from port 0, stores and retrieves from RAM, then writes to port 0.
  * In between writing and reading, the accumulator is set to zero and written to port 0 to prove the
  * value was stored and retreived successfully from memory.
+ * Requires input and output at port 0
  */
 //byte ROM[64] = {
 //  0xDB, 0x00,        // IN A, 0: Load val from port 0 into accumulator (11011011, 00000000)
@@ -62,6 +64,43 @@ byte ROM[64] = {
 //  0x7E,              // LD A, (HL): Read value from memory into accumulator (01111110)
 //  0xD3, 0x00,        // OUT 0, A: Write accumulator to port 0 (11010011, 00000000)
 //  0xC3, 0x00, 0x00   // JP 0: Jump to start (11000011, 00000000, 00000000)
+//};
+
+/* 
+ * Memory and output checkout with fibonacci. Repeatedly outputs value one, value two, then the sum.
+ * Requires only output at port 0.
+ */
+//byte ROM[64] = {
+//  0x3E, 0x00,        // LD A, 0: Load zero into accumulator
+//  0x32, 0x00, 0x80,  // LD (0x8000), A: Load accumulator into RAM start
+//  0x3E, 0x01,        // LD A, 1: Load 1 into accumulator
+//  0x32, 0x01, 0x80,  // LD (0x8001), A: Load accumulator into RAM byte 1
+//  0x3E, 0x00,        // LD A, 0: Load 0 into accumulator
+//  0xD3, 0x00,        // OUT 0, A: Write accumulator to port 0
+//  // Loop start
+//  // Retrieve values from memory
+//  0x26, 0x80,        // LD H, 0x80: Load MSB of RAM start into register H
+//  0x2E, 0x00,        // LD L, 0: Load LSB of RAM start into register L
+//  0x7E,              // LD A, (HL): Read value from memory into accumulator
+//  0xD3, 0x00,        // OUT 0, A: Write accumulator to port 0
+//  0x47,              // LD B, A: Load value from accumulator into register B
+//  0x26, 0x80,        // LD H, 0x80: Load MSB of RAM byte 1 into register H
+//  0x2E, 0x01,        // LD L, 0: Load LSB of RAM byte 1 into register L
+//  0x7E,              // LD A, (HL): Read value from memory into accumulator
+//  0xD3, 0x00,        // OUT 0, A: Write accumulator to port 0
+//  0x4F,              // LD C, A: Load value from accumulator into register C
+//  // Add new total and display
+//  0x80,              // ADD A, B: Add value from register B to accumulator
+//  0xD3, 0x00,        // OUT 0, A: Write accumulator to port 0
+//  // Save registers to memory
+//  0x26, 0x80,        // LD H, 0x80: Load MSB of RAM start into register H
+//  0x2E, 0x00,        // LD L, 0: Load LSB of RAM start into register L
+//  0x71,              // LD (HL), B: Load value from register C into memory
+//  0x26, 0x80,        // LD H, 0x80: Load MSB of RAM byte 1 into register H
+//  0x2E, 0x01,        // LD L, 0: Load LSB of RAM byte 1 into register L
+//  0x77,              // LD (HL), A: Load value from accumulator into memory
+//  // Jump to start of loop
+//  0xC3, 0x0E, 0x00  // JP 14: Jump to line 14
 //};
 
 //==============================================================================
